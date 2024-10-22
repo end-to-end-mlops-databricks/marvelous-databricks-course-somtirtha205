b2ea99a53eda8d11e3d24a0865d1eedc0a8e6a57
somtirtha205 <somtirtha205>
2024-10-22 03:37:44 +0000

    Data Processing Final Notebook

---
+++ notebooks/Data_Processing_Final.py
@@ -0,0 +1,33 @@
+# Databricks notebook source
+# MAGIC %pip install /Volumes/mlops_dev/hotel_reservation/data/hotel_reservation-0.0.1-py3-none-any.whl
+
+# COMMAND ----------
+
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
+import yaml
+
+from hotel_reservation.data_processor import DataProcessor
+
+# Load configuration
+with open('../project_config.yml', 'r') as file:
+    config = yaml.safe_load(file)
+
+print("Configuration loaded:")
+print(yaml.dump(config, default_flow_style=False))
+
+# COMMAND ----------
+
+# Initialize DataProcessor
+data_processor = DataProcessor('/Volumes/mlops_dev/hotel_reservation/data/Data.csv', config)
+
+# COMMAND ----------
+
+# Preprocess the data
+data_processor.preprocess_data()
+
+# COMMAND ----------
+
+

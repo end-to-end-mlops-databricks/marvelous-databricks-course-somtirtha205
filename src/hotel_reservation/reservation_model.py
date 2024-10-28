7433ea6b9f8ddd8c0c6512110ddb342084166185
somtirtha205 <somtirtha@yahoo.com>
2024-10-28 18:30:13 -0400

    Minor change in parameters in model

--- src/hotel_reservation/reservation_model.py
+++ src/hotel_reservation/reservation_model.py
@@ -15,7 +15,7 @@ class ReservationModel:
                         n_estimators=config["parameters"]["n_estimators"],
                         max_depth=config["parameters"]["max_depth"],
                         learning_rate=config["parameters"]["learning_rate"],
-                        random_state=42,
+                        random_state=config["parameters"]["random_state"],
                     ),
                 ),
             ]
